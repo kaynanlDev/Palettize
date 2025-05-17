ec9c89f619bcf00d23f72d80c6e8d45cb0ae8e8e
Kaynan Teixeira <167570173+kaynanlDev@users.noreply.github.com>
2025-05-17 22:51:42 +0000

    criação e estilização de layout

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,35 @@
-<script setup></script>
+<script setup>
+import Cards from "./components/Cards.vue";
+</script>
 
 <template>
-  <h1>hello</h1>
+  <header><v-img src="assets/"></v-img></header>
+  <main>
+    <h1>Gerador de paleta de cores</h1>
+    <Cards />
+  </main>
+  <footer>
+    <span>&copy;Todos os direitos reservados - 2025</span>
+  </footer>
 </template>
 
 <style scoped lang="scss">
-body {
+header {
   width: 100%;
+  padding: 24px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+main {
+  width: 100%;
+  padding: 24px;
 
   h1 {
-    color: red;
+    color: #69247c;
+    font-size: 40px;
+    text-align: center;
   }
 }
 </style>
---
+++ src/components/Cards.vue
@@ -0,0 +1,40 @@
+<script setup></script>
+<template>
+  <div class="cards">
+    <div class="card">
+      <div class="card-color"></div>
+      <p>#000</p>
+    </div>
+    <div class="card">
+      <div class="card-color"></div>
+      <p>#000</p>
+    </div>
+    <div class="card">
+      <div class="card-color"></div>
+      <p>#000</p>
+    </div>
+    <div class="card">
+      <div class="card-color"></div>
+      <p>#000</p>
+    </div>
+  </div>
+</template>
+<style scoped lang="scss">
+.cards {
+  max-width: 70%;
+  margin: 24px auto;
+  display: flex;
+  justify-content: center;
+  gap: 16px;
+
+  .card-color {
+    width: 100%;
+    height: 100%;
+    background-color: #d9d9d9;
+  }
+  P {
+    color: #69247c;
+    font-weight: 700;
+  }
+}
+</style>
